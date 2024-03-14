e9971c89d62c18d3875627d9bac86ada0e3813e8
Mehta, Niraj Yogendra <nmehta90@gatech.edu>
2024-03-14 13:46:01 -0700

    basic read infrastructure

--- read_cfdb.py
+++ read_cfdb.py
@@ -2,25 +2,30 @@ import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
 
-api_key = os.getenv('API_KEY')
+def read_db(endpoint, params):
+    load_dotenv()
+    return requests.get('https://api.collegefootballdata.com' + endpoint,
+                        params=params,
+                        headers={'accept': 'application/json',
+                                 'Authorization': 'Bearer {}'.format(os.getenv('API_KEY'))})
 
-base_url = 'https://api.collegefootballdata.com'
-endpoint = '/teams/fbs'
-params = {'year': '2023'}
-headers = {
-    'accept': 'application/json',
-    'Authorization': f'Bearer {api_key}'
-}
 
-response = requests.get(base_url + endpoint, params=params, headers=headers)
+class CFDB:
+    def __init__(self):
+        endpoint = '/teams/fbs'
+        params = {'year': '2023'}
+        response = read_db(endpoint, params)
 
-if response.status_code == 200:
-    data = response.json()
-    for team in data:
-        print("School:", team["school"])
-        print("Conference:", team["conference"])
-        print()
-else:
-    print('Error:', response.status_code)
+        if response.status_code == 200:
+            data = response.json()
+            for team in data:
+                print("School:", team["school"])
+                print("Conference:", team["conference"])
+                print()
+        else:
+            print('Error:', response.status_code)
+
+
+if __name__ == "__main__":
+    CFDB()
