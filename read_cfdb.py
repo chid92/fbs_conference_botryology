508d117a6c7374d985c5492bca96ee8f83c0cc2d
Mehta, Niraj Yogendra <nmehta90@gatech.edu>
2024-03-14 16:54:15 -0700

    add conference and team data to cfdb class

--- read_cfdb.py
+++ read_cfdb.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 
 def read_db(endpoint, params):
-    load_dotenv()
     return requests.get('https://api.collegefootballdata.com' + endpoint,
                         params=params,
                         headers={'accept': 'application/json',
@@ -13,19 +12,9 @@ def read_db(endpoint, params):
 
 class CFDB:
     def __init__(self):
-        endpoint = '/teams/fbs'
-        params = {'year': '2023'}
-        response = read_db(endpoint, params)
-
-        if response.status_code == 200:
-            data = response.json()
-            for team in data:
-                print("School:", team["school"])
-                print("Conference:", team["conference"])
-                print()
-        else:
-            print('Error:', response.status_code)
-
-
-if __name__ == "__main__":
-    CFDB()
+        load_dotenv()
+        self.conferences = ['ACC', 'Big Ten', 'Big 12', 'Pac-12', 'SEC']
+        self.teams = [{'school': team['school'],
+                       'conference': team['conference']}
+                      for team in read_db('/teams/fbs', {'year': '2023'}).json()
+                      if team['conference'] in self.conferences]
