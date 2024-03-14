679b19db5635390ec7d2baa20330dddf7e533a07
Mehta, Niraj Yogendra <nmehta90@gatech.edu>
2024-03-14 13:08:26 -0700

    test reading from cfbd

--- read_cfdb.py
+++ read_cfdb.py
@@ -1,7 +1,22 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 api_key = os.getenv('API_KEY')
-headers = {'Authorization': f'Bearer {api_key}'}
+
+base_url = 'https://api.collegefootballdata.com'
+endpoint = '/teams/fbs'
+params = {'year': '2023'}
+headers = {
+    'accept': 'application/json',
+    'Authorization': f'Bearer {api_key}'
+}
+
+response = requests.get(base_url + endpoint, params=params, headers=headers)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print('Error:', response.status_code)
