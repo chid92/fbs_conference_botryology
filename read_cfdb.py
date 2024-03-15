8be1f6fbb378a1c0dfb05e75605452ef42ebf4bd
Mehta, Niraj Yogendra <nmehta90@gatech.edu>
2024-03-15 16:51:19 -0700

    code cleanup

--- read_cfdb.py
+++ read_cfdb.py
@@ -10,7 +10,7 @@ def read_db(endpoint, params):
     return requests.get('https://api.collegefootballdata.com' + endpoint,
                         params=params,
                         headers={'accept': 'application/json',
-                                 'Authorization': 'Bearer {}'.format(os.getenv('API_KEY'))})
+                                 'Authorization': f'Bearer {os.getenv("API_KEY")}'})
 
 
 class CFDB:
@@ -21,16 +21,12 @@ class CFDB:
                        'conference': team['conference']}
                       for team in read_db('/teams/fbs', {'year': '2023'}).json()
                       if team['conference'] in self.conferences]
-        for team in read_db('/teams/fbs', {'year': '2023'}).json():
-            print(team['conference'])
-        self.teams.append({'school': 'Notre Dame',
-                           'conference': 'FBS Independents'})
-        self.teams.append({'school': 'Connecticut',
-                           'conference': 'FBS Independents'})
-        self.teams.append({'school': 'SMU',
-                           'conference': 'American Athletic'})
-        self.teams.append({'school': 'Tulane',
-                           'conference': 'American Athletic'})
+        self.teams.extend([
+            {'school': 'Notre Dame', 'conference': 'FBS Independents'},
+            {'school': 'Connecticut', 'conference': 'FBS Independents'},
+            {'school': 'SMU', 'conference': 'American Athletic'},
+            {'school': 'Tulane', 'conference': 'American Athletic'}
+        ])
 
     def write_matchups_csv(self, filename):
         if os.path.exists(filename):
@@ -39,16 +35,15 @@ class CFDB:
         progress_bar = tqdm(total=math.comb(len(self.teams), 2), desc="Reading matchups")
 
         df_data = []
-        for i in range(len(self.teams) - 1):
-            for j in range(i + 1, len(self.teams)):
-                team1 = self.teams[i]['school']
-                team2 = self.teams[j]['school']
-                data = read_db('/teams/matchup', {'team1': team1,
-                                                  'team2': team2,
+        for i, team1 in enumerate(self.teams[:-1]):
+            for team2 in self.teams[i + 1:]:
+                data = read_db('/teams/matchup', {'team1': team1['school'],
+                                                  'team2': team2['school'],
                                                   'minYear': '1869',
                                                   'maxYear': '2069'}).json()
-                games = data['team1Wins'] + data['team2Wins'] + data['ties']
-                df_data.append({'team1': team1, 'team2': team2, 'games': games})
+                df_data.append({'team1': team1['school'],
+                                'team2': team2['school'],
+                                'games': data['team1Wins'] + data['team2Wins'] + data['ties']})
                 progress_bar.update(1)
 
         pd.DataFrame(df_data).to_csv(filename)
