a5c436e0a214884f3a79a5ae9f9441885fbc74fb
Mehta, Niraj Yogendra <nmehta90@gatech.edu>
2024-03-15 19:20:26 -0700

    initial partitioning attempt

--- cfb_teams_graph.py
+++ cfb_teams_graph.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import networkx as nx
+import itertools
+import random
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 class CFGraph:
@@ -25,3 +28,42 @@ class CFGraph:
         plt.savefig('graph.png',
                     format='png',
                     bbox_inches='tight')
+
+    def partition_graph(self, num_partitions, num_iterations=1000):
+        nodes = list(self.graph.nodes())
+        random.shuffle(nodes)
+        partitions = [nodes[i::num_partitions] for i in range(num_partitions)]
+        progress_bar = tqdm(total=num_iterations, desc="Reading matchups")
+
+        for _ in range(num_iterations):
+            improved = False
+            for i, j in itertools.combinations(range(num_partitions), 2):
+                cut = nx.algorithms.cuts.cut_size(self.graph, partitions[i], partitions[j], weight='weight')
+
+                for u in partitions[i]:
+                    for v in partitions[j]:
+                        new_partitions = [partitions[p][:] for p in range(num_partitions)]
+                        new_partitions[i].remove(u)
+                        new_partitions[j].remove(v)
+                        new_partitions[i].append(v)
+                        new_partitions[j].append(u)
+
+                        if nx.algorithms.cuts.cut_size(self.graph,
+                                                       new_partitions[i],
+                                                       new_partitions[j],
+                                                       weight='weight') < cut:
+                            partitions = new_partitions
+                            improved = True
+                            break
+                    if improved:
+                        break
+                if improved:
+                    break
+            if not improved:
+                break
+            progress_bar.update(1)
+
+        for i, partition in enumerate(partitions):
+            print(f"Partition {i + 1}: {partition}")
+
+        return partitions
--- conference_botryology.py
+++ conference_botryology.py
@@ -4,4 +4,8 @@ from cfb_teams_graph import CFGraph
 if __name__ == '__main__':
     cfdb = CFDB()
     cfdb.write_matchups_csv('matchups.csv')
+
+    print("Generating graph")
     cfgraph = CFGraph('matchups.csv')
+    print("Partitioning")
+    cfgraph.partition_graph(9)
